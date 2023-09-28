7c8afb51a488f12666b0f0516ec5978a1681b38d
ynotaman <146350867+ynotaman@users.noreply.github.com>
2023-09-28 18:55:59 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,67 @@
+'''#use for next line
+print("hey i am a good boy \n this is my own laptopn")
+#use separate
+print("hey~7~aman")
+#variable
+a=1
+print(a)
+a=2
+b=4
+print(b+a)
+str = "hello"
+print(str)
+list = ["aman"],[4823894]
+print(list)
+dict={"Name = " "amaan","age = 20","add=ara" }
+print(dict)
+print(8**3)
+d=4
+c=7
+e=c+d
+print(e)
+f="23"
+g="34"
+print(g+f)
+print(int(f)+int(g))
+string = "15"
+numner = 78
+str=(int(string))
+sum=str+numner
+print(sum)
+print(type(a))
+print(type(str))
+print(type(string))
+x=input()
+print("my name is",x)
+y=input()
+z=input()               
+w=(int(z+y))
+#print("the sum is :",int(w))
+print("the sum is:",int(y)+int(z))
+print(int(y)//int(z))
+name="aman"
+friend="Imp"
+fri="lovely"
+#print(name)
+print(name[0:3])
+friut="mango"
+print(len(friut))
+print(friut[0:5])
+print(friut[-1:5])
+print(friut[-2:4])
+print(friut[4])'''
+a="amaan Waris!!!"
+#print(len(a))
+#print(a.rstrip("!!!"))
+print(a.rstrip("!"),a.upper())
+#print(a.rstrip("!"))
+#print(a.replace,a.rstrip("amaan","waris"))
+'''print(a.replace("amaan","waris"))
+print(a.split(" ")) # ask to sir
+blog="mohammad Amaan WaRIS"
+print(blog.capitalize())
+#print(blog.center(45),capitalize()) ask to sir...............................a
+print(blog.count("a"))
+print(blog.center(67))
+print(len(blog))'''
+
